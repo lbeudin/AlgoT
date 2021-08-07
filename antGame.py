70035cef2f0af0cf71d552fe5811d2dd5347fafc
lbeudin <36150586+lbeudin@users.noreply.github.com>
2021-08-07 13:43:06 +0100

    Update antGame.py

--- antGame.py
+++ antGame.py
@@ -89,3 +89,8 @@ antGameStart(number_simulation,length_tab,secondForAMove,True)
 Average step :  25.788792  average steps sec :  257.88792
 Average step :  35.459197  average steps sec :  354.59197000000006
 """
+"""
+Two ants start in opposite corners of a regular chessboard. Every 10 seconds, they move from the center of the square they're on to the center of an adjacent square.
+How long until they both land on the same square? How long until their paths cross (Ant A moving from square K to L and Ant B moving from square L to K)?
+What happens if we allow the ants to move diagonally? What happens if we restrict ants from moving to their immediately previous square?
+"""
